87ac0fca98fee338803ca6d6f697c0a009f2e384
roblanf <rob.lanfear@gmail.com>
2020-05-02 15:47:35 +1000

    Create filter_aln.sh
    filters alignments as follows:
    
    remove seqs with len<29400 and >20 ambiguities
    remove sites with > 0.1% gaps

---
+++ scripts/filter_aln.sh
@@ -0,0 +1,37 @@
+
+helpFunction()
+{
+   echo "Filter strains and columns from an alignment"
+   echo "Usage: $0 -i fasta_alignment -o output_fasta -t threads"
+   echo "\t-i Full path to aligned fasta file of SARS-CoV-2 sequences"
+   echo "\t-o Output file path for filtered fasta"
+   echo "\t-t number of threads to use"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:o:t:" opt
+do
+   case "$opt" in
+      i ) inputfasta="$OPTARG" ;;
+      o ) outputfasta="$OPTARG" ;;
+      t ) threads="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$inputfasta" ] || [ -z "$outputfasta" ] || [ -z "$threads" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+echo ""
+echo "Filtering strains shorter than 29400 and/or with more than 20 ambiguities"
+echo "Filtering sites with more than 0.1% gaps"
+echo ""
+
+esl-alimanip --lmin 29400 --xambig 20 --informat afa --outformat afa --dna -o $inputfasta $inputfasta"_alimanip.fa" 
+esl-alimask --gapthresh 0.001 --informat afa --outformat afa --dna -o $outputfasta -g  $inputfasta"_alimanip.fa"
+
+rm $inputfasta"_alimanip.fa"
