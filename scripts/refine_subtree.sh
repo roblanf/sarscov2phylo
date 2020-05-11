8c715e35f504b6597a8c113b1ec00e15937a2fae
roblanf <rob.lanfear@gmail.com>
2020-05-11 21:01:37 +1000

    make it useable with options
    also now makes a rudimentary pdf

--- scripts/refine_subtree.sh
+++ scripts/refine_subtree.sh
@@ -1,54 +1,58 @@
+#!/bin/bash
 
+helpFunction()
+{
+   echo "Use ML to refine the sub-tree of a focal taxon"
+   echo "Usage: $0 -i fasta_alignment -t threads -g global_phylogeny -f focal_taxon_name -d depth"
+   echo "    -i Full path to aligned fasta file of SARS-CoV-2 sequences"
+   echo "    -t number of threads to use"
+   echo "    -g global phylogeny that contains the focal taxon"
+   echo "    -f focal taxon name (must be present in alignment and global phylogeny)"
+   echo "    -d number of substitutions below the focal taxon to prune the sub-tree"
+   exit 1 # Exit script after printing help
+}
 
+while getopts "i:t:g:f:d:" opt
+do
+   case "$opt" in
+      i ) aln="$OPTARG" ;;
+      t ) threads="$OPTARG" ;;
+      g ) tree="$OPTARG" ;;
+      f ) seq="$OPTARG" ;;
+      d ) depth="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
 
-# input the taxa you want to refine a tree for
-
-# also the global trees you want to use
-
-# also the depth you want to go from each focal taxon
-
-# get the tree N levels deep - could just keep increasing N until we have at least 100 sequences
-
-nw_clade -c 5 global.fa_mp_boot_TBE.tree 'ACT01_consensus_sequence'
-
-# to get how many taxa 
-nw_clade -c 50 global.fa_mp_boot_TBE.tree 'ACT01_consensus_sequence' | nw_stats -
-
-# to get how deep we've gone from the focal leaf
-nw_clade -c 40 global.fa_mp_boot_TBE.tree 'ACT01_consensus_sequence' | nw_distance - 'ACT01_consensus_sequence'
-
-
-
-depth=3
-tree=global.fa.parstree
-aln=global.fa
-seq=ACT01_consensus_sequence
-threads=50
+# Print helpFunction in case parameters are empty
+if [ -z "$inputfasta" ] || [ -z "$threads" ] || [ -z "$tree" ] || [ -z "$seq" ] || [ -z "$depth" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
 
 # calculate the minimum depth we need to get to 
 length=$(esl-alistat $aln | grep 'Alignment length' | cut -d ':' -f2 | tr -d '[:space:]')
-depth_bl=$( bc -l <<< '$depth/$length')
+depth_bl=$( bc -l <<< "$depth/$length")
 
 
 # get the depth we need to go to in order to get the right number of substitutions
 # we just keep going one parent deeper until the distance from the focal leaf
 # to the parent node is at least depth/alignment_lenght
 for c in $(seq 1 100); do
-	depth=$(nw_clade -c $c $tree $seq | nw_distance - $seq)
+    depth=$(nw_clade -c $c $tree $seq | nw_distance - $seq)
 
-	# convert depth to a float
-	printf -v depth "%f" "$depth"
+    # convert depth to a float
+    printf -v depth "%f" "$depth"
 
-	if (( $(bc -l <<< "$depth > $depth_bl") )); then
-		break
-	fi
+    if (( $(bc -l <<< "$depth > $depth_bl") )); then
+        break
+    fi
 done
-
 
 # make a sub-alignment
 # the nw_ commands just get the sub-tree, get the names
 # then we cut and pass to faSomeRecords
-
 nw_clade -c $c $tree $seq | nw_distance -n -s f - | cut -f1 | faSomeRecords $aln /dev/stdin $seq'_aln.fa'
 
 # add the outgroup sequencde to the alignment
@@ -67,11 +71,18 @@ bash $DIR/tree_ml.sh -i $seq'_aln.fa' -t $threads
 finalTBE=$seq'_aln.fa_ml_boot_TBE.tree'
 finalFBP=$seq'_aln.fa_ml_boot_FBP.tree'
 
-# display the tree
-echo '"<circle style=’fill:red;stroke:black’ r=’5’/>" I "'$seq'"' > ornament.map
-nw_display -s -o ornament.map $finalTBE > $finalTBE'.svg'
+# make rudimentary pdfs
+echo "fill:blue L "$seq > css.map
+echo '"stroke-width:2; stroke:blue"  Clade '$seq >> css.map
+
+nw_display -s -w 1000 -c css.map $finalTBE > $finalTBE'.svg'
 inkscape -f $finalTBE'.svg' -D -A $finalTBE.pdf
 
+nw_display -s -w 1000 -c css.map $finalFBP > $finalFBP'.svg'
+inkscape -f $finalTBE'.svg' -D -A $finalFBP.pdf
+
+rm *.tax
 
 # dating the tree
 #../iqtree-2.0.4-Linux/bin/iqtree2 -s global.fa -te global.fa_mp_boot_TBE.tree -keep-ident -m JC -fixbr --date TAXNAME -o "hCoV-19/Wuhan/WH04/2020|EPI_ISL_406801|2020-01-05" -nt 4 -pre date
+
