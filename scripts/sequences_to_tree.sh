5a79a38354f0faa92c977a16ee28779ae4244288
roblanf <rob.lanfear@gmail.com>
2020-05-11 21:59:28 +1000

    full monty

--- scripts/sequences_to_tree.sh
+++ scripts/sequences_to_tree.sh
@@ -106,8 +106,8 @@ echo ""
 
 # finally, we estimate a tree with 100 bootstraps, using rapidnj, MP, and fasttree
 bash $DIR/tree_nj.sh -i $outputfasta -t $threads
-#bash $DIR/tree_mp.sh -i $outputfasta -t $threads
-#bash $DIR/tree_ft.sh -i $outputfasta -t $threads
+bash $DIR/tree_mp.sh -i $outputfasta -t $threads
+bash $DIR/tree_ft.sh -i $outputfasta -t $threads
 
 # now we refine trees for all of the sequences we're really interested in.
 declare -a to_refine=$(grep '>' $addseqs | tr -d \>)
