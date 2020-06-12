00bc312f7b0be1b4384a3700dbb4634fd6abffdd
roblanf <rob.lanfear@gmail.com>
2020-06-12 16:33:52 +1000

    halfway changs to making the global tree
    not yet functional

--- scripts/global_tree_gisaid.sh
+++ scripts/global_tree_gisaid.sh
@@ -41,39 +41,34 @@ cat $inputfasta > $allseqs
 
 
 echo ""
-echo "Processing raw data and trimming UTRs "
+echo "Cleaning raw data"
 echo ""
 
-trimmed_gisaid="$inputdir/trimmed.fa"
-bash $DIR/trim_seqs.sh -i $allseqs -o $trimmed_gisaid -t $threads
+cleaned_gisaid=$inputfasta"_cleaned.fa"
+bash $DIR/trim_seqs.sh -i $allseqs -o $cleaned_gisaid -t $threads
 
 #### BUILD THE GLOBAL ALIGNMENT ######
-
-# first align the k most dissimilar sequences, a low k is sensible here
-# e.g. 100. Higher numbers slow down the global alignment step
-echo ""
-echo "Making alignment of $k most dissimilar sequences"
-echo ""
-aln_k="$inputdir/aln_k.fa"
-bash $DIR/align_k_dissimilar.sh -i $trimmed_gisaid -k $k -o $aln_k -t $threads
-
-echo ""
-echo "Filtering sites with >10% gaps from k most dissimilar sequence alignment"
-echo ""
-aln_k_filtered="$inputdir/aln_k_filtered.fa"
-esl-alimask --gapthresh 0.1 --informat afa --outformat afa --dna -o $aln_k_filtered -g  $aln_k
-
 
 echo ""
 echo "Making global profile alignment"
 echo ""
-aln_global="$inputdir/aln_global_unfiltered.fa"
-bash $DIR/global_profile_alignment.sh -i $trimmed_gisaid -o $aln_global -t $threads -r $aln_k_filtered
-
+aln_global="$inputdir/aln_global_unmasked.fa"
+bash $DIR/global_profile_alignment.sh -i $cleaned_gisaid -o $aln_global -t $threads
 
 echo ""
-echo "Filtering alignment"
+echo "Masking alignment"
 echo ""
+
+aln_global_masked="$inputdir/aln_global_masked.fa"
+bash $DIR/mask_alignment.sh -i $aln_global -o $aln_global_masked -t $threads
+
+
+
+
+
+
+
+
 
 echo "alignment stats before filtering"
 esl-alistat $aln_global
