a5debdd2f199aff0b8de4cd97ae8709c5b67ac24
roblanf <rob.lanfear@gmail.com>
2020-09-03 11:27:35 +1000

    fix bugs in excluded seqs update

--- scripts/update_excluded_seqs.R
+++ scripts/update_excluded_seqs.R
@@ -3,7 +3,9 @@
 # usage: Rscript update_excluded_seqs.R old_iteration_file treeshrink_file
 args = commandArgs(trailingOnly=TRUE)
 
-old = args[1]
+old_file = args[1]
+old = read.delim(old_file, header=F)
+
 treeshrink = args[2]
 e = read.delim(treeshrink, header=F)
 e = t(e)
@@ -13,4 +15,4 @@ f = data.frame(cbind(e, "on long branch identified by treeshrink q=0.05"))
 names(f)=names(old)
 new = rbind(old, f)
 
-write.table(new, file="excluded_sequences.tsv", sep="\t", col.names = F)
+write.table(new, file="excluded_sequences.txv", sep="\t", col.names = F, row.names = F)
