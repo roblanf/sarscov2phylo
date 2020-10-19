b64e9dfcc3904bfcb2890811d95c70491d7e5542
roblanf <rob.lanfear@gmail.com>
2020-10-19 13:49:12 +1100

    ?

--- scripts/prune_terminals.R
+++ scripts/prune_terminals.R
@@ -20,19 +20,14 @@ drop.terminals <- function(tree, mutations=5){
     terminal.edgelengths = tree$edge.length[branchSel]
     
     newtree=tree
-    i=1
     while(max(terminal.edgelengths*30000 > mutations)){
         # the true alignment length is just under 30K, so this is equivalent to
         # getting all branches >=mutations mutations
-        print(newtree)
         long.terminals = which(terminal.edgelengths*30000 > mutations)
 
         newdropped = data.frame(name = newtree$tip.label[long.terminals], length = terminal.edgelengths[long.terminals]*30000)
         dropped = rbind(dropped, newdropped)
 
-        print(tail(dropped))
-        print(i)
-        i=i+1
         
         # the indices of terminal.edgelengths and tree$tip.label are the same, meaning this works
         newtree = drop.tip(newtree, long.terminals)
@@ -46,19 +41,13 @@ drop.terminals <- function(tree, mutations=5){
 }
 
 r = drop.terminals(tree)
-print("-1")
 newtree = r[[1]]
-print("0")
 dropped.tips = r[[2]]
 
-print("a")
 dropped.tips$length = paste("terminal branch length of ~", as.integer(dropped.tips[,2]), " mutations", sep="")
 
-print(args[2])
-print(newtree)
-
-write.tree(newtree, "test123.tree")
-
-print("c")
+#?
+t2 = drop.tip(tree, droppeed.tips$name)
+write.tree(t2, args[2])
 
 write.table(dropped.tips, file="excluded_sequences.tsv", sep="\t", col.names = F, row.names = F, quote=F)
