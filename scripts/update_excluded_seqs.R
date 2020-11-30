22f7939af893b1642445b64d9a9a2fdd20f1c91b
roblanf <rob.lanfear@gmail.com>
2020-11-30 16:05:02 +1100

    fix bug that creates empty file
    the bug appears when treeshrink doesn't exlude any new sequences. in this case the code would produce an empty excluded_sequences.tsv file.

--- scripts/update_excluded_seqs.R
+++ scripts/update_excluded_seqs.R
@@ -7,12 +7,16 @@ old_file = args[1]
 old = read.delim(old_file, header=F)
 
 treeshrink = args[2]
-e = read.delim(treeshrink, header=F)
-e = t(e)
-e = e[complete.cases(e)]
-f = data.frame(cbind(e, "on long branch identified by treeshrink q=0.05"))
 
-names(f)=names(old)
-new = rbind(old, f)
+if (file.info(treeshrink)$size < 2) {
+    new = old
+} else {
+    e = read.delim(treeshrink, header=F)
+    e = t(e)
+    e = e[complete.cases(e)]
+    f = data.frame(cbind(e, "on long branch identified by treeshrink q=0.05"))
+    names(f)=names(old)
+    new = rbind(old, f)
+}
 
 write.table(new, file="excluded_sequences.tsv", sep="\t", col.names = F, row.names = F, quote=F)
